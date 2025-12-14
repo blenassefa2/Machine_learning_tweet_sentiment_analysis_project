f669eb4eab8ae74365867efa0f63e0e4fcb69347
blenassefa2 <blenassefa59@gmail.com>
2025-12-14 12:18:23 +0100

    fix: labeling keyword utilization

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
-from app.routers import datasets, session, clean, label
+from app.routers import datasets, session, clean, label, train
 
 
 port = int(os.environ.get("PORT", 8080))
@@ -30,7 +30,7 @@ app.add_middleware(
 app.include_router(datasets.router)
 app.include_router(clean.router)
 app.include_router(label.router)
-# app.include_router(train.router, prefix="/api/train", tags=["Train"])
+app.include_router(train.router)
 # app.include_router(predict.router, prefix="/api/predict", tags=["Predict"])
 # app.include_router(evaluate.router, prefix="/api/evaluate", tags=["Evaluate"])
 # app.include_router(user.router, prefix="/api/user", tags=["user"])
